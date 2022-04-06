77d786ec62947573546881664a8b2c9f1165af14
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-04-06 21:21:05 +0800

    Initial commit and set up of svelte and tailwind

---
+++ client/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h1>Hello Typescript!</h1>
+
+  <!-- <Counter /> -->
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <button class="py-3 bg-red-500">Tailwind complete</button>
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
+
+<style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
